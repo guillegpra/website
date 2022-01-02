cd30f06f8c83e5cebff16e8173d48e775d8c27d3
guillermogarciapradales <guillegpra@gmail.com>
2022-01-02 16:02:24 +0100

    Adding teams and contact section

--- script.js
+++ script.js
@@ -13,6 +13,28 @@ $(document).ready(() => {
         $('.menu-btn').toggleClass('active');
         $('.menu-btn i').toggleClass('active');
     });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
 });
 
-// min 1:11:07 https://www.youtube.com/watch?v=tcskp-ncN0I
+// min 1:45:34 https://www.youtube.com/watch?v=tcskp-ncN0I
